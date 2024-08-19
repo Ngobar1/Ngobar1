bb6c8447315c274f9a53a9835547c5b44ce7bc3a
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-08-19 21:34:44 +0700

    setup

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['./tests/setup/setupTestEnvironment.js'],
+  moduleNameMapper: {
+    '^src/(.*)': '<rootDir>/src/$1',
+  },
+
+  moduleDirectories: ['node_modules', 'src'],
+};
