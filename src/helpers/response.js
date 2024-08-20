22f2766ba7d5a696c91b74977585f8fc962241d7
nugrahaAdhitama <nugrahaadhitama22@gmail.com>
2024-08-20 23:03:34 +0700

    feat: Add response helper for sending API responses

---
+++ src/helpers/response.js
@@ -0,0 +1,22 @@
+exports.sendResponse = (
+  res,
+  status,
+  message,
+  data = null,
+  authorization = null,
+) => {
+  const layout = {
+    status,
+    message,
+  };
+
+  if (data) {
+    layout.data = data;
+  }
+
+  if (authorization) {
+    layout.authorization = authorization;
+  }
+
+  return res.status(status).json(layout);
+};
